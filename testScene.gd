935345940f28e5eca9ddaf9b7192be892221f03f
Lucas C Oliveira <33939027+lucascoliveira189@users.noreply.github.com>
2020-05-10 16:27:40 -0300

    attempting to modify and get data from the Database, init project

--- testScene.gd
+++ testScene.gd
@@ -1,5 +1,57 @@
 extends Control
 
+var firebase = null
 
 func _ready() -> void:
-	print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+	print("################## Start App ##################")
+
+	if Engine.has_singleton("Firebase"):
+		firebase = Engine.get_singleton("Firebase")
+
+	if firebase:
+		firebase.init(get_instance_id())
+
+		var dic: Dictionary = {"nome": "Lucas", "sobrenome": "C Oliveira"}
+
+		firebase.firestore_add_document("FirstCollection", dic)
+		firebase.firestore_load_document("FirstCollection")
+		firebase.firestore_set_document_data("FirstCollection", "FirstDocument", dic)
+
+
+		print("instance_id: ", get_instance_id())
+
+		print("________________ CODE END ________________")
+		print(" ")
+
+
+func _on_firebase_receive_message(tag, from, key, data) -> void:
+
+	if tag == "Firebase":
+		if from == "Firestore":
+			if key == "AddDocument":
+				print("################## AddDocument ##################")
+				print("data: ", data)
+			
+			if key == "Documents":
+				print("################## Documents ##################")
+				print("data: ", data)
+
+			if key == "SetDocument":
+				print("################## SetDocument ##################")
+				print("data: ", data)
+
+			# if key == "Document":
+			# 	print("################## Document ##################")
+			# 	print("data: ", data)
+
+			# if key == "LoadDocument":
+			# 	print("################## LoadDocument ##################")
+			# 	print("data: ", data)
+
+	# print("############################")
+	# print("tag: ", tag)
+	# print("from: ", from)
+	# print("key: ", key)
+	# print("data: ", data)
+	# print("############################")
+	# print(" ")
